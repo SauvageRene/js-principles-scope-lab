7ae48144647205008a69e14eb865016372b941d9
Rene <renegarcia1864@gmail.com>
2021-05-11 19:45:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "somebody i used to know"
+
+
+function upperCaseCustomerName() {
+     customerName = customerName.toUpperCase();
+    //  No return statement, because it will not redefine the variable!
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+     bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "billy"
+}
